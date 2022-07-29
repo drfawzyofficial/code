db9445cbf48d4ff53b16e90b1599c7d3e706c9f8
Abdulrahman Fawzy <36601080+fawzyeltop@users.noreply.github.com>
2022-07-29 16:12:47 +0200

    Revert "Indexjs"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const getData = async () => {
+async function getData() {
     const response= await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita%')
     console.log(response);
     const data= await response.json();
