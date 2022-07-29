96bc46a362cc2e137a9d952bc1f66c5ccfde1ee1
Abdulrahman Fawzy <AbdulrahmanFawzy999@gmail.com>
2022-07-29 15:04:41 +0200

    add all files

---
+++ index.js
@@ -0,0 +1,10 @@
+async function getData() {
+    const response= await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita%')
+    console.log(response);
+    const data= await response.json();
+    console.log(data);
+    length=data.drinks.length;
+    console.log(data);
+  }
+  getData(); 
+
